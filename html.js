66663fc8187e128910a1ec3ee2b986824fe89056
seonhop <78877741+seonhop@users.noreply.github.com>
2021-10-25 02:40:45 -0400

    dd

---
+++ html.js
@@ -0,0 +1,12 @@
+// if (document.readyState == 'loading') {
+//     document.addEventListener('DOMContentLoaded', ready)
+// } else {
+//     ready()
+// }
+
+// function ready() {
+//     var select = document.getElementById("glazing-option");
+//     var option = select.options[select.selectedIndex];
+//     console.log(option.text);
+// }
+
